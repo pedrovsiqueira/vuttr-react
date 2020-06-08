4fc6ec0438e89f99ba2101b535fe35999d09d3bc
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-08 11:02:07 -0300

    Create Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>VUTTR</h1>
+      <h3>Very Useful Tools to remember</h3>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/styles/global.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  button{
+    cursor: pointer;
+    border: none;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  input{
+    border: none
+  }
+
+  
+
+
+`;
